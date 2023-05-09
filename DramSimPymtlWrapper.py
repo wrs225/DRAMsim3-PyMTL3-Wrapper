d8c3395a97bc21f64ab72cb47eab44d97c6c3c94
wrs225 <wrs225@cornell.edu>
2023-05-09 12:14:57 -0400

    code cleanliness

--- DramSimPymtlWrapper.py
+++ DramSimPymtlWrapper.py
@@ -1,3 +1,10 @@
+#---------------------------------------------------
+#
+#   DramSimPyMTLWrapper.py
+#   Will Salcedo '23
+#
+#---------------------------------------------------
+
 from pymtl3 import *
 
 from .DRAMsimWrapper.memory_system import *
@@ -23,7 +30,7 @@ class DramSimPymtlWrapper( Component ):
     #DRAM CLK is 1333MHz, let's say our system is 4000MHz
     def construct(s, RTL_CLK_PER = 3, INPUT_CONFIG = u'../DDR3_1Gb_x8_1333.ini', OUTPUT_CONFIG = u'./', mem_nwords = (1_000_000_000 // 8) // 8):
         
-        s.istream= stream.ifcs.RecvIfcRTL( MemMsg )
+        s.istream = stream.ifcs.RecvIfcRTL( MemMsg )
         s.ostream = stream.ifcs.SendIfcRTL( MemMsg )
 
         # Define two queues with depth 1
@@ -32,12 +39,12 @@ class DramSimPymtlWrapper( Component ):
 
         #assign queues to interface
         s.queue1.recv //= s.istream
-        s.ostream //= s.queue2.send
+        s.ostream     //= s.queue2.send
         
         #Wires so I can divide the clock and see the status of DRAMsim3 stalling in simulation.
         #in_flight is needed to hold state. This module is a weird combo of hardware and software.
-        s.in_flight = Wire( Bits1 )
-        s.memsystem_rdy = Wire( Bits1 )
+        s.in_flight      = Wire( Bits1 )
+        s.memsystem_rdy  = Wire( Bits1 )
         s.rtl_clk_cycles = Wire( Bits32 ) 
         
         #Bytearray to hold values we store.
@@ -51,7 +58,7 @@ class DramSimPymtlWrapper( Component ):
         def increment():
             
 
-            s.queue2.recv.msg.w @= s.queue1.send.msg.w 
+            s.queue2.recv.msg.w    @= s.queue1.send.msg.w 
             s.queue2.recv.msg.addr @= s.queue1.send.msg.addr
 
             if s.in_flight:
@@ -63,47 +70,48 @@ class DramSimPymtlWrapper( Component ):
             
             #function call in combinational block is sus to me. 
             if s.queue1.send.msg.w and s.in_flight:
+
                 s.queue2.recv.msg.msg @= s.queue1.send.msg.msg
+
                 write_bytearray_bits(s.mem, s.queue1.send.msg.addr, 8, s.queue1.send.msg.msg)
+
             else:
+
                 s.queue2.recv.msg.msg @= read_bytearray_bits(s.mem, s.queue1.send.msg.addr, 8)
 
             
         #Clocked block. DRAM clock division & adding transactions to DRAM model
         @update_ff
         def block():
+
             if(s.reset):
+
                 s.rtl_clk_cycles <<= Bits32(0)
+
             else:
+
                 if(s.rtl_clk_cycles == RTL_CLK_PER):
+
                     s.drammodel.ClockTick()
                     s.rtl_clk_cycles <<= Bits32(0)
+
                 else:
+
                     s.rtl_clk_cycles <<= s.rtl_clk_cycles + Bits32(1)
                     
             s.memsystem_rdy <<= s.drammodel.WillAcceptTransaction(s.queue1.send.msg.addr * 64, bool(s.queue1.send.msg.w))
+
             if (not s.in_flight) and s.queue1.send.val and s.queue2.recv.rdy and s.drammodel.WillAcceptTransaction(s.queue1.send.msg.addr * 64, bool(s.queue1.send.msg.w)):
+               
                 s.in_flight <<= Bits1(1)
+                
                 s.drammodel.AddTransaction(s.queue1.send.msg.addr * 64, bool(s.queue1.send.msg.w))
+                
             else:
+
                 s.in_flight <<= Bits1(0)
             
             
-
-                
-
-                   
-                    
-
-                
-
-
-
-        
-
-
-
-
 @bitstruct
 class MemMsg:
     w:    Bits1
